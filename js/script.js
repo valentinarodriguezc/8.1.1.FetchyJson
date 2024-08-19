4e77d5667a2aaa6f27c2a0d68e51cae283f20e62
valentinarodriguezc <valentinarodriguezcarvallido@gmail.com>
2024-08-19 00:26:10 -0300

    fetch-show-names

--- js/script.js
+++ js/script.js
@@ -19,3 +19,9 @@ function showData(dataArray) {
 }
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+fetch(DATA_URL)
+  .then(response => response.json())
+  .then(data => {
+    showData(data.students);
+  })
+  .catch(error => console.error('Error al cargar los datos:', error));
